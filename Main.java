a0307173ef2280fd3f26d3621b004d9c0ffb823f
Satish Gavhane <satishgavhane2909@gmail.com>
2024-06-15 22:45:31 +0530

    Bug 1 fixed

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ package Git_Practice;
 public class Main {
     public static void main(String[] args) {
         String name = "Satish";
+        int bug1 = 23;
+        System.out.println(bug1+" is fixex");
     }
 }
